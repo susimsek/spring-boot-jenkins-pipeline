f7fd7a1695dc8dc6979a098ebd850972beeaf24f
Şuayb Şimşek <suayb.simsek@detaysoft.com>
2020-03-26 17:44:37 +0300

    add

---
+++ src/main/java/com/SpringBootJenkinsPipelineApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJenkinsPipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJenkinsPipelineApplication.class, args);
+    }
+
+}
